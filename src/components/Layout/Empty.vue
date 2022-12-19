5bd0a2fb047308236faadde11f5dcb115fa7c5df
rikkapro0128 <stkssc002@gmail.com>
2022-12-19 20:55:38 +0700

    save code

--- src/components/Layout/Empty.vue
+++ src/components/Layout/Empty.vue
@@ -15,18 +15,51 @@
               <div :class="`relative h-full transition-transform`"
                 :style="{ width: `${nav.maxWidth}px`, transform: `translateX(${toggle ? 0 : -100}%)` }">
                 <div class="px-3">
-                  <div>
-                    <p class="text-bold">Kiểu đọc</p>
-                    <el-button
-                      @click="options.typeRead.index = handleNext(options.typeRead.index, options.typeRead.val.length)"
-                      class="w-full mt-2 uppercase" type="success">{{
-                          options.typeRead.val[options.typeRead.index]
-                      }}</el-button>
-                  </div>
-                  <div>
-                    <p class="text-bold">Tốc độ kéo</p>
-                    <el-slider v-model="options.speedDrag" :step="0.5" max="5" show-stops />
-                  </div>
+                  <el-space class="w-full" fill wrap direction="vertical" :size="10">
+                    <div>
+                      <p class="text-bold">Kiểu đọc</p>
+                      <el-button
+                        @click="options.typeRead.index = handleNext(options.typeRead.index, options.typeRead.val.length)"
+                        class="w-full mt-2 uppercase" type="success">{{
+                            options.typeRead.val[options.typeRead.index]
+                        }}</el-button>
+                    </div>
+                    <div>
+                      <p class="text-bold">Tốc độ kéo</p>
+                      <el-slider v-model="options.speedDrag" :step="0.5" max="5" show-stops
+                        style="--el-slider-main-bg-color: var(--el-color-success);" />
+                    </div>
+                    <div>
+                      <p class="text-bold">Chapter</p>
+                      <el-select v-model="selectChapter" class="w-full mt-2" placeholder="Select" size="large">
+                        <el-option v-for="chapter in chapters" :key="chapter.id"
+                          :label="chapter.num + ' - ' + chapter.name" :value="chapter.num" />
+                      </el-select>
+                    </div>
+                    <div>
+                      <p class="text-bold">Trang</p>
+                      <el-select v-model="selectChapter" class="w-full mt-2" placeholder="Select" size="large">
+                        <el-option v-for="chapter in chapters" :key="chapter.id"
+                          :label="chapter.num + ' - ' + chapter.name" :value="chapter.num" />
+                      </el-select>
+                    </div>
+                    <div>
+                      <div class="flex justify-between flex-nowrap">
+                        <p class="text-bold">Lưu lịch sử đọc</p>
+                        <el-switch v-model="options.historySave" size="small"
+                          style="--el-switch-on-color: var(--el-color-success);" />
+                      </div>
+                    </div>
+                    <div>
+                      <div class="flex justify-between flex-nowrap">
+                        <p class="text-bold">Cuộn tự động</p>
+                        <el-switch v-model="options.autoScroll.state" size="small"
+                          style="--el-switch-on-color: var(--el-color-success);" />
+                      </div>
+                      <el-slider v-model="options.autoScroll.pick" :step="10" show-stops class="mt-2 transition-all"
+                        :style="{ '--el-slider-main-bg-color': 'var(--el-color-success)', 'transform': `translateY(${options.autoScroll.state ? '0' : '-12px'})`, 'opacity': `${options.autoScroll.state ? 1 : 0}` }" />
+                    </div>
+                  </el-space>
                 </div>
               </div>
             </div>
@@ -70,7 +103,46 @@ const options = reactive({
     index: 0,
   },
   speedDrag: 0,
+  autoScroll: {
+    state: false,
+    pick: 30,
+  },
+  historySave: false,
 })
+
+const chapters = reactive([
+  {
+    id: 1,
+    num: 1,
+    name: 'No name 1',
+    link: 'https://element-plus.org/en-US/component/select.html',
+  },
+  {
+    id: 2,
+    num: 1.1,
+    name: 'No name 2',
+    link: 'https://element-plus.org/en-US/component/select.html',
+  },
+  {
+    id: 3,
+    num: 1.2,
+    name: 'No name 3',
+    link: 'https://element-plus.org/en-US/component/select.html',
+  },
+  {
+    id: 4,
+    num: 1.3,
+    name: 'No name 4',
+    link: 'https://element-plus.org/en-US/component/select.html',
+  },
+  {
+    id: 5,
+    num: 1.4,
+    name: 'No name 5',
+    link: 'https://element-plus.org/en-US/component/select.html',
+  },
+]);
+const selectChapter = ref(chapters[0].num);
 
 const handleNext = (index, length) => {
   return index === length - 1 ? 0 : ++index;
